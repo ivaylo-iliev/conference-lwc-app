ac449cfa3408d1a13efbbec6b89868eaee98f15c
Ivaylo Iliev <ivailo.iliev@nextforcedev.com>
2020-05-28 14:32:42 +0300

    Added navigation

--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -14,4 +14,13 @@ export default class SessionList extends LightningElement {
             session.name.toLowerCase().includes(searchKey)
         );
     }
+
+    handleSessionClick(event) {
+        const index = event.currentTarget.dataset.index;
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: this.sessions[index].id
+        });
+
+        this.dispatchEvent(navigateEvent);
+    }
 }
